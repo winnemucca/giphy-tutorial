89a6ec4e356dc113542c8c23dbe934368906ba19
Steven Kauyedauty <skauyedauty@gmail.com>
2019-10-30 04:11:37 -0600

    spec file modified

--- src/app/gipy-service.service.spec.ts
+++ src/app/gipy-service.service.spec.ts
@@ -1,12 +1,17 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, inject } from '@angular/core/testing';
 
 import { GiphyService } from './gipy-service.service';
-import { HttpClientModule } from '@angular/common/http';
+
+import {
+  HttpClientTestingModule,
+  HttpTestingController
+} from '@angular/common/http/testing';
+import { HttpEvent, HttpEventType } from '@angular/common/http/http';
 
 describe('GiphyService', () => {
   beforeEach(() => TestBed.configureTestingModule({
     imports: [
-      HttpClientModule
+      HttpClientTestingModule
     ]
   }));
 
@@ -14,4 +19,23 @@ describe('GiphyService', () => {
     const service: GiphyService = TestBed.get(GiphyService);
     expect(service).toBeTruthy();
   });
+
+  // it(
+  //   'should get users',
+  //   inject(
+  //     [HttpTestingController, GiphyService],
+  //     (httpMock: HttpTestingController, giphyService: GiphyService) => {
+  //       const searchTerm = {
+  //         term: 'test'
+  //       };
+
+  //       // giphyService.getResults(searchTerm).subscribe((event: HttpEvent<any>) => {
+  //       //   switch (event.type) {
+  //       //     case HttpEventType.Response:
+  //       //       expect(event.body).toEqual(searchTerm);
+  //       //   }
+  //       // });
+  //     }
+  //   )
+  // );
 });
